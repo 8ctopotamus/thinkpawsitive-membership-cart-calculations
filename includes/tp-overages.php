6ed81869572e58e46d85cfa210c6a58573fbbd13
8ctopotamus <joshnaylor88@gmail.com>
2018-12-09 16:29:00 -0600

    Data based on WC_Booking post-type instead of wc_orders

--- includes/tp-overages.php
+++ includes/tp-overages.php
@@ -37,17 +37,18 @@
     endforeach;
   }
 
-  function renderDateNav($moStart) {
-    // nav link dates
+  function renderDateNavLinks($moStart) {
     $navDate = new DateTime( date("Y-m", $moStart) );
     $navDate->modify( '-1 month' );
     $prev = $navDate->format( 'Y-m' );
     $navDate->modify( '+2 month' );
     $next = $navDate->format( 'Y-m' );
     ?>
-      <a class="date-nav-link" href="<?php echo admin_url('admin.php?page=tp_membership_overages&date=' . $prev); ?>"><<</a>
-      <?php echo date("F Y", $moStart); ?>
-      <a class="date-nav-link" href="<?php echo admin_url('admin.php?page=tp_membership_overages&date=' . $next); ?>">>></a>
+      <div class="tp-date-nav">
+        <a class="date-nav-link" href="<?php echo admin_url('admin.php?page=tp_membership_overages&date=' . $prev); ?>"><span class="dashicons dashicons-arrow-left-alt2"></span></a>
+        <?php echo date("F Y", $moStart); ?>
+        <a class="date-nav-link" href="<?php echo admin_url('admin.php?page=tp_membership_overages&date=' . $next); ?>"><span class="dashicons dashicons-arrow-right-alt2"></span></a>
+      </div>
     <?php
   }
 
@@ -62,22 +63,24 @@
         <?php foreach($memberships as $membership): ?>
           <span class="tp-membership-label <?php echo determineMembershipBGColor($membership->plan->name); ?>"><?php echo $membership->plan->name; ?></span>
         <?php endforeach; ?>
-        <p><a href="mailto: <?php echo $email; ?>"><?php echo $email; ?></a></p>
+        <a href="mailto: <?php echo $email; ?>"><?php echo $email; ?></a>
         <hr/>
         <?php foreach($products_by_cat as $cat => $products):
           $prod_count = count($products);
           $limit = get_membership_cat_limit($cat, $memberships);
           ?>
-          <div class="tp-cat-group">
-            <h4 class="tp-cat-group-title">
-              <?php echo $cat . ' - ' . $prod_count . '/' . $limit; ?>
-              <?php echo renderOverageWarning($prod_count, $limit); ?>
-            </h4>
-            <ol>
-              <?php foreach($products as $product):?>
-                <li><?php echo $product->get_name(); ?> <a href="<?php echo admin_url( 'post.php?post=' . absint( $product->order_id ) . '&action=edit' ); ?>" target="_blank">View order</a></li>
-              <?php endforeach; ?>
-            </ol>
+          <div class="tp-cat-grid">
+            <div class="tp-cat-group">
+              <h4 class="tp-cat-group-title">
+                <?php echo $cat . ' - ' . $prod_count . '/' . $limit; ?>
+                <?php echo renderOverageWarning($prod_count, $limit); ?>
+              </h4>
+              <ol>
+                <?php foreach($products as $product):?>
+                  <li><?php echo $product->get_name(); ?> <a href="<?php echo admin_url( 'post.php?post=' . absint( $product->order_id ) . '&action=edit' ); ?>" target="_blank">View order</a></li>
+                <?php endforeach; ?>
+              </ol>
+            </div>
           </div>
         <?php endforeach; ?>
       </div>
@@ -85,89 +88,70 @@
   }
 
   function tp_get_bookings($start, $end) {
+    $RUNNING_TOTAL = array();
     $WCBookings = new WP_Query(array(
       'post_type' => 'wc_booking',
+      'post_status' => array('complete', 'paid', 'processing'),
       'posts_per_page' => -1,
       'date_query' => array(
         'after' => date("Y-n-j", $start),
-        'before' => date("Y-n-j", $end)
+        'before' => date("Y-n-j", $end),
+        'inclusive' => true,
       ),
     ));
     if ( $WCBookings->have_posts() ) :
     	while ( $WCBookings->have_posts() ) : $WCBookings->the_post();
         $booking = new WC_Booking( get_the_id() );
+        // nice_var_dump($booking);
         // $current_timestamp = $booking->get_start_date();
         // nice_var_dump($current_timestamp);
         // nice_var_dump( $booking->get_product() );
-        nice_var_dump( $booking->get_customer() );
-    	endwhile;
-    	wp_reset_postdata();
-    else :
-    	echo 'No bookables found.';
-    endif;
-
-    // loop over past month's orders - OLD
-    $order_statuses = array('wc-on-hold', 'wc-processing', 'wc-completed');
-    $past_orders = wc_get_orders(array(
-      // 'date_query' => date("Y-n-j", strtotime("first day of previous month")),
-      'date_query' => array(
-        'after' => date("Y-n-j", strtotime("first day of previous month")),
-        'before' => date("Y-n-j", strtotime("last day of previous month"))
-      ),
-      'post_status' => $order_statuses,
-      'numberposts' => -1,
-    ));
-
-    // data store
-    $RUNNING_TOTAL = array();
-
-    foreach ($past_orders as $order):
-      $user = $order->get_user();
-      $user_id = $user->ID;
-      // we only care about users who are members
-      $memberships = wc_memberships_get_user_active_memberships( $user_id );
-      if (empty($memberships)) {
-        continue;
-      } else {
+        // nice_var_dump( $booking->get_customer() );
+        // nice_var_dump( $booking->get_order() );
+        $user_id = $booking->get_customer()->user_id;
+        $memberships = wc_memberships_get_user_active_memberships( $user_id );
+        if (empty($memberships)) {
+          continue;
+        } else {
         // add user to $RUNNING_TOTAL if doesn't exist
         if ( !isset( $RUNNING_TOTAL[$user_id] ) ):
           $RUNNING_TOTAL[$user_id] = array(
-            'name' => $user->display_name,
-            'email' => $user->user_email,
+            'id' => $booking->get_customer()->user_id,
+            'name' => $booking->get_customer()->name,
+            'email' => $booking->get_customer()->email,
             'memberships' => $memberships,
             'products_by_cat' => array(),
           );
         endif;
-        // add products to user obj
-        foreach($order->get_items() as $item_id => $item):
-          $product = wc_get_product( $item['product_id'] );
-          // skip if not a wc bookable product
-          if (!$product->is_type( 'booking' )) {
-            continue;
-          }
-          // check if product category ids are in membership rules
-          $product_cats = $product->get_category_ids();
-          foreach($_SESSION['category_ids'] as $cat_name => $cat_id):
-            $matches = array_intersect($cat_id, $product_cats);
-            if ($matches) {
-              // add order date to each product for later use
-              $product->order_id = $order->get_id();
-              // add to our member
-              if (!isset($RUNNING_TOTAL[$user_id]['products_by_cat'][$cat_name])) {
-                $RUNNING_TOTAL[$user_id]['products_by_cat'][$cat_name] = array($product);
-              } else {
-                array_push($RUNNING_TOTAL[$user_id]['products_by_cat'][$cat_name], $product);
-              }
+        // check if product category ids are in membership rules
+        $product = $booking->get_product();
+        $product_cats = $product->get_category_ids();
+        $order_id = $booking->get_order()->get_id();
+        foreach($_SESSION['category_ids'] as $cat_name => $cat_id):
+          $matches = array_intersect($cat_id, $product_cats);
+          if ($matches) {
+            // add order date to each product for later use
+            $product->order_id = $order_id;
+            // add to our member
+            if (!isset($RUNNING_TOTAL[$user_id]['products_by_cat'][$cat_name])) {
+              $RUNNING_TOTAL[$user_id]['products_by_cat'][$cat_name] = array($product);
+            } else {
+              array_push($RUNNING_TOTAL[$user_id]['products_by_cat'][$cat_name], $product);
             }
-          endforeach;
+          }
         endforeach;
       }
-    endforeach;
-    return $RUNNING_TOTAL;
+    	endwhile;
+    	wp_reset_postdata();
+      return $RUNNING_TOTAL;
+    else :
+    	echo 'No bookables found.';
+      return false;
+    endif;
   }
 
   function tp_membership_overages_page_template() {
-    // parse the date for bookings query
+    // parse the date
     if (isset($_GET['date'])) {
       $moStart = strtotime("first day of" . $_GET['date']);
       $moEnd = strtotime("last day of" . $_GET['date']);
@@ -179,14 +163,15 @@
       <div class="wrap tp-membership-overages">
         <h1 class="tp-membership-overages-page-title">
           <?php echo esc_html( get_admin_page_title() ); ?>
-          <small><?php renderDateNav( $moStart ); ?></small>
+          <small><?php renderDateNavLinks( $moStart ); ?></small>
         </h1>
         <?php
           $RUNNING_TOTAL = tp_get_bookings($moStart, $moEnd);
-
-          foreach($RUNNING_TOTAL as $member):
-            renderMemberTemplate($member);
-          endforeach;
+          if ($RUNNING_TOTAL):
+            foreach($RUNNING_TOTAL as $member):
+              renderMemberTemplate($member);
+            endforeach;
+          endif;
         ?>
       </div>
     <?php
